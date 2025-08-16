bf0c63c4dc52e9746757a454734f5637105efe92
Fireking13 <144738550+Fireking13@users.noreply.github.com>
2025-08-16 02:11:47 -0400

    ui started

---
+++ Source/ThrowingGame/Public/Game/UIBGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "UIBGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THROWINGGAME_API UUIBGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
